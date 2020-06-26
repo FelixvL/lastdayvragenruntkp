384fc096f7ea2d263c056328e5f8f698b0fa9ea2
FelixvL <digitaleservice@gmail.com>
2020-06-26 12:18:33 +0200

    mv

--- Demo.java
+++ Demo.java
@@ -1,26 +1,13 @@
-class Demo{   //GB
+class Demo{   //MV
 	static public void main(String args[]){
-		r35 jojo = new r35(54);
-		System.out.println( watnouweer(jojo).tt24()  );
+
 	}
-	static b52 watnouweer(b52 abc){
-		return abc;
-	}
+
 }
 
 
-class r35 extends b52{
-	r35(int a){
-		super(a);
-	}
-	int tt24(){
-		return 6;
-	}
-}
-class b52{
-	b52(int b){
-		System.out.println(b);
-	}
+class Dier{
+	abstract void gaan();
 }
 
 
