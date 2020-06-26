c078ec249bd557e50cfc30b4f2ae90dc6052121f
FelixvL <digitaleservice@gmail.com>
2020-06-26 09:31:59 +0200

    tb

--- Demo.java
+++ Demo.java
@@ -1,8 +1,15 @@
-class Demo{   //LD
+class Demo{   //TB
 	int a = kopen();
+	static int b = kopen();
 	static public void main(String rbc[]){
-		System.out.println("hoi");
-		new Demo().lopen();
+		Demo demo1 = new Demo();
+		b = 35;
+		demo1.a = 25;
+		Demo demo2 = new Demo();
+		System.out.println(demo1.a);
+		System.out.println(demo1.b);
+		System.out.println(demo2.a);
+		System.out.println(demo2.b);
 	}
 	static void lopen(){
 		System.out.println("in lopen");
