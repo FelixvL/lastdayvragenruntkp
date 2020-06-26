7c91dda8e748359e00764187646f516a9426deaa
FelixvL <digitaleservice@gmail.com>
2020-06-26 09:56:26 +0200

    bq

--- Demo.java
+++ Demo.java
@@ -1,12 +1,12 @@
-class Demo{   //EX
+class Demo{   //BQ
 	static public void main(String rbc[]){
 		new Stoel().zitten(45);
 	}
 }
 class Stoel{
 	String zitten;
-	void zitten(int do){
-		System.out.println("zitten in stoel")
+	void zitten(int doen){
+		System.out.println("zitten in stoel");
 	}
 	void zitten(char go){
 		System.out.println("zitten in chair");
