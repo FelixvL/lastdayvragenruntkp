c8c2ac38b304cffb9d5dc16769195a93381acd61
FelixvL <digitaleservice@gmail.com>
2020-06-26 09:24:49 +0200

    ld

--- Demo.java
+++ Demo.java
@@ -1,10 +1,15 @@
-class Demo{   //OG
+class Demo{   //LD
+	int a = kopen();
 	static public void main(String rbc[]){
 		System.out.println("hoi");
 		new Demo().lopen();
 	}
-	void lopen(){
+	static void lopen(){
 		System.out.println("in lopen");
+	}
+	static int kopen(){
+		System.out.println("in kopen");
+		return 7;
 	}
 
 }
