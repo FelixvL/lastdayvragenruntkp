211b125616bfe114781c19da0fb5407c9f66ee0a
FelixvL <digitaleservice@gmail.com>
2020-06-26 09:59:56 +0200

    pl

--- Demo.java
+++ Demo.java
@@ -1,13 +1,21 @@
-class Demo{   //BQ
+class Demo{   //PL
 	static public void main(String rbc[]){
-		new Stoel().zitten(45);
+		int nummer = 45;
+		kijken(nummer);
+		Stoel stoel = new Stoel();
+		kijken(stoel);
+		System.out.println(nummer);
+		System.out.println(stoel.zitten);
+	}
+	static void kijken(int getal){
+		getal = 35;
+	}
+	static void kijken(Stoel chair){
+		chair.zitten = 22;
 	}
 }
-class Stoel{
-	String zitten;
-	void zitten(int doen){
-		System.out.println("zitten in stoel");
-	}
+public class Stoel{
+	int zitten = 11;
 	void zitten(char go){
 		System.out.println("zitten in chair");
 	}
