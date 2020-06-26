e93681695f530dfec56f1f0e2245e7584d26d896
FelixvL <digitaleservice@gmail.com>
2020-06-26 13:31:43 +0200

    bt

--- Demo.java
+++ Demo.java
@@ -1,30 +1,13 @@
-class Demo{   //EX
+class Demo{   //BT
 	static public void main(String args[]){
-		try{
-			checkit().gaan();
-			System.out.println("ik doe mee");
-		}catch(Exception e){
-			System.out.println("gered");
-		}
-		System.out.println("begin");
-	}
-	static Dier checkit(){
-		return new Mier();
-	}
-}
-abstract class Dier{ 
-	void gaan(){
-		System.out.println("gaan in Dier");
-	}
-}
+		int a = 8;
+		if(a < 5)
+			System.out.println("yes");
+			System.out.println("no");
+		System.out.println("einde");
 
-class Mier extends Dier{
-	void gaan() throws Error{
-		System.out.println("gaan in mier");
-		throw new RuntimeException();
 	}
 }
-
 
 
 
