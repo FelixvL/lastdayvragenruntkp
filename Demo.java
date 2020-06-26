4bfe069f9fc37d010ac75531673f34f4069ff5b9
FelixvL <digitaleservice@gmail.com>
2020-06-26 11:26:42 +0200

    mb

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
-class Demo{   //ND
+class Demo{   //MB
 	static public void main(String rbc[]){
-		// Wat is de jdk?
+		// Wat is de jdk? Java Development Kit -> produceren programmeurs
+		// Wat is de jre?
 	}
 
 
