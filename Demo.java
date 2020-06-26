ebfd268a9964d07bb261eeb9a8aa914a358aee59
FelixvL <digitaleservice@gmail.com>
2020-06-26 11:30:12 +0200

    mk

--- Demo.java
+++ Demo.java
@@ -1,7 +1,10 @@
-class Demo{   //MB
+class Demo{   //MK
 	static public void main(String rbc[]){
 		// Wat is de jdk? Java Development Kit -> produceren programmeurs
-		// Wat is de jre?
+		// Wat is de jre? Java Runtime Environment -> runnen afspelen iederecomputer  3 billion
+		// JVM Java Virtual Machine
+
+//		Java is hierop trots en zegt, onze taal is ..........
 	}
 
 
