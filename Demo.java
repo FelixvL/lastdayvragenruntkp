d1bc8e8407c2b9871cdb78917db8760536d4e930
FelixvL <digitaleservice@gmail.com>
2020-06-26 10:11:22 +0200

    zp

--- Demo.java
+++ Demo.java
@@ -1,21 +1,16 @@
-class Demo{   //PK
+class Demo{   //ZP
 	static public void main(String rbc[]){
-		int nummer = 45;
-		kijken(nummer);
-		Stoel stoel = new Stoel();
-		kijken(stoel);
-		System.out.println(nummer);
-		System.out.println(stoel.zitten);
+		int nummer = 019;
+		zitten(nummer);
+
 	}
-	static void kijken(int getal){
-		getal = 35;
+	static void zitten(int abc){
+		char vr = (char)abc;
+		new Stoel().zitten(vr);
 	}
-	static void kijken(Stoel chair){
-		chair.zitten = 22;
-	}
+
 }
 class Stoel{
-	int zitten = 11;
 	void zitten(char go){
 		System.out.println("zitten in chair");
 	}
