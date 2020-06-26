bf67dca03e8665ad80b76d071e19897e33b2fc93
FelixvL <digitaleservice@gmail.com>
2020-06-26 10:48:25 +0200

    om

--- Demo.java
+++ Demo.java
@@ -1,16 +1,17 @@
-class Demo{   //JJ
+class Demo{   //OM
 	static public void main(String rbc[]){
-		new Stoel();
+		new Stoel(2);
 	}
 
 
 }
 class Stoel{
-	final static int hoogte;
-	Stoel(){
+	int[] array = new int[3]{3,7,8};
+	Stoel(int i){
+		System.out.println(array[i]);
 		System.out.println("c in Stoel");
 	}
 	Stoel deStoel(){
-		return null;
+		return new Stoel(1);
 	}
 }
