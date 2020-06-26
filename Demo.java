26bfbdbba4572f892c1a667875e939c6785e7717
FelixvL <digitaleservice@gmail.com>
2020-06-26 09:48:41 +0200

    wv

--- Demo.java
+++ Demo.java
@@ -1,11 +1,17 @@
-class Demo{   //YA
+class Demo{   //WV
 	static public void main(String rbc[]){
-		lopen(null);
+
 	}
 	static void lopen(String tekst){
-		System.out.println("in lopen"+tekst);
-		char e = 70;
+	
 	}
+}
+class Stoel{
+	String merk;
+	void zitten(){
 
+	}
+	static void zitten(){
 
+	}
 }
