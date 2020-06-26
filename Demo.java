180fcbc5e1e627e0130895f86567cafcba81fabe
FelixvL <digitaleservice@gmail.com>
2020-06-26 13:19:09 +0200

    et

--- Demo.java
+++ Demo.java
@@ -1,26 +1,27 @@
-class Demo{   //OO
+class Demo{   //ET
 	static public void main(String args[]){
-		checkit().gaan();
+		try{
+			checkit().gaan();
+			System.out.println("ik doe mee");
+		}catch(Exception e){
+			System.out.println("gered");
+		}
+		System.out.println("begin");
 	}
 	static Dier checkit(){
 		return new Mier();
 	}
-
 }
-
-
 abstract class Dier{ 
 	void gaan(){
 		System.out.println("gaan in Dier");
-	}
-	Dier(){
-		System.out.println("I am Dier");	
 	}
 }
 
 class Mier extends Dier{
 	void gaan() throws Error{
 		System.out.println("gaan in mier");
+		throw new Error();
 	}
 }
 
