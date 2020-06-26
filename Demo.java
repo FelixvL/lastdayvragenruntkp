4f0fdf488c09968bea81acc720c09b10e797414d
FelixvL <digitaleservice@gmail.com>
2020-06-26 10:42:09 +0200

    jj

--- Demo.java
+++ Demo.java
@@ -1,7 +1,6 @@
-class Demo{   //RZ
+class Demo{   //JJ
 	static public void main(String rbc[]){
 		new Stoel();
-		return;
 	}
 
 
@@ -9,7 +8,6 @@ class Demo{   //RZ
 class Stoel{
 	final static int hoogte;
 	Stoel(){
-		this.hoogte = 23;
 		System.out.println("c in Stoel");
 	}
 	Stoel deStoel(){
