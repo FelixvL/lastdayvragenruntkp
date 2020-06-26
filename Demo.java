fe6847f5bc52596ce6d4b766900805fdabb9f72c
FelixvL <digitaleservice@gmail.com>
2020-06-26 11:25:03 +0200

    nd

--- Demo.java
+++ Demo.java
@@ -1,16 +1,8 @@
-class Demo{   //OW 
+class Demo{   //ND
 	static public void main(String rbc[]){
-		Computer s = new Server();
-		System.out.println(s.watt);
+		// Wat is de jdk?
 	}
 
 
 }
-class Computer{
-	float watt = 220.4f;
-}
 
-class Server extends Computer{
-	float watt = 220.7f;
-	String url = "nos.nl";
-}
