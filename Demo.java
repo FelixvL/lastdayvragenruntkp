b08755dd7cae37ca73cc9fafa5f04ba9d6cfb9a1
FelixvL <digitaleservice@gmail.com>
2020-06-26 09:52:25 +0200

    ex

--- Demo.java
+++ Demo.java
@@ -1,17 +1,14 @@
-class Demo{   //WV
+class Demo{   //EX
 	static public void main(String rbc[]){
-
-	}
-	static void lopen(String tekst){
-	
+		new Stoel().zitten(45);
 	}
 }
 class Stoel{
-	String merk;
-	void zitten(){
-
+	String zitten;
+	void zitten(int do){
+		System.out.println("zitten in stoel")
 	}
-	static void zitten(){
-
+	void zitten(char go){
+		System.out.println("zitten in chair");
 	}
 }
