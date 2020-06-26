17ee6310e9caa11f4bcef555da4b4e6bdb1832e0
FelixvL <digitaleservice@gmail.com>
2020-06-26 15:14:35 +0200

    lq

--- Demo.java
+++ Demo.java
@@ -1,17 +1,21 @@
-class Demo{    // LF
+class Demo{    // LQ
 	static public void main(String args[]){
-		new Roos().looper();
+		try{
+			new Roos().looper();
+		}catch(Exception e){
+		}
+		System.out.println("einde");
 	}
 }
 class Roos{
 		
 	int[] getallen = {12,14,5,23,41};
 	void looper(){
-		for(int i = 0 ; i < getallen.length; i++){
+		for(int i = 0 ;   ; i++){
 			if(getallen[i] == 14){
 				throw new RuntimeException();
 			}else{	
-				i++;
+				i += getallen[i] % i;
 			}
 			System.out.println(getallen[i]);
 		}
