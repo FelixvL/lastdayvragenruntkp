06d6a6dcaed85c3960b3811435aa1a5b07765943
FelixvL <digitaleservice@gmail.com>
2020-07-06 16:02:50 +0200

    US

--- Demo.java
+++ Demo.java
@@ -1,37 +1,14 @@
-class Demo{    // ZZ
-	static int a = vult();
-	static int vult(){
-		System.out.println("vult in demo");
-		return 3;
+class Demo{  //US
+	 public static void main(String[] args){
+		System.out.println("Uitsmijter2");
+		new Uitsmijter().checkit();
 	}
-	{
-		System.out.println("er wordt een demo gemaakt");
+}
+class Uitsmijter{
+	void checkit(int... get){
+		System.out.println("in checkit");
 	}
-	static public void main(String args[]){
-		Meer meer = new Meer(2);
-		Boot boot = new Boot();
-		boot.naam = "Kameleon";
-		new Demo().leegpompen(meer);
-		new Demo().leegpompen(meer);
-		new Demo().leegpompen(meer);
-		System.out.println(">>"+boot.naam);
-		if(boot.naam == meer.boot.naam){
-			System.out.println("yes" + meer.diepte);
-		}
+}
 
-	}
-	void leegpompen(Meer meertje){
-		meertje = new Meer(5);
-		meertje.boot = new Boot();
-	}
-}
-class Meer{
-	static Boot boot;
-	int diepte;
-	Meer(int diepte){
-		this.diepte = diepte;
-	}	
-}
-class Boot{
-	String naam = "abc";
-}
+
+
